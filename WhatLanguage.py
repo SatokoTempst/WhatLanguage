f89c4a7c6d8ab0caaeb2619a659b84b45054c03c
SatokoTempest <satokotempest@gmail.com>
2023-06-14 10:58:51 +0330

    Add files via upload

---
+++ WhatLanguage.py
@@ -0,0 +1,25 @@
+def Program():
+    x =input("What language are you learning right now ?")
+    if "Python" or "python" in x:
+        print("Python is Cool.Keep grinding GG(Get Gud XD).")
+    elif "C++" or "cpp" and "Cpp" in x:
+        print("C++ is for Chads.Respect!")
+    elif "C#" or "csharp" and "Csharp" in x:
+        print("C# hmm I don't know much about it Lol")
+    elif "Java" or "java"  in x:
+            print("Java one Time write it . Run it everywhere HaHa!")
+    elif "JavaScript" and "javascript" and "JS" in x:
+        print("You can't Run from ME :D ")
+    else :
+        print("Unkown Language.Try Again!")
+        y =(1,)
+        for z in y:
+            Program()
+            
+            break
+
+
+Program()
+
+
+    
