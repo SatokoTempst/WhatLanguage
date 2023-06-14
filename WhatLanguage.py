fc16144b02b749784391224b1921a51b5ba4e47b
SatokoTempest <satokotempest@gmail.com>
2023-06-14 11:35:28 +0330

    Add files via upload

--- WhatLanguage.py
+++ WhatLanguage.py
@@ -1,14 +1,14 @@
 def Program():
     x =input("What language are you learning right now ?")
-    if "Python" or "python" in x:
+    if "Python" in x:
         print("Python is Cool.Keep grinding GG(Get Gud XD).")
-    elif "C++" or "cpp" and "Cpp" in x:
+    elif "C++" in x:
         print("C++ is for Chads.Respect!")
-    elif "C#" or "csharp" and "Csharp" in x:
+    elif "C#" in x:
         print("C# hmm I don't know much about it Lol")
-    elif "Java" or "java"  in x:
-            print("Java one Time write it . Run it everywhere HaHa!")
-    elif "JavaScript" and "javascript" and "JS" in x:
+    elif "Java" in x:
+        print("Java one Time write it . Run it everywhere HaHa!")
+    elif "JS" in x:
         print("You can't Run from ME :D ")
     else :
         print("Unkown Language.Try Again!")
